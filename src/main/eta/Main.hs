bcbb57f63c450160bf75d85e14aee2d7cdae2712
Eunmin Kim <telepopsound@gmail.com>
2019-10-03 00:17:04 +0900

    Set up eta Gradle project structure

---
+++ src/main/eta/Main.hs
@@ -0,0 +1,3 @@
+import Primes
+
+main = putStrLn $ "The 101st prime is " ++ show (primes !! 100)
---
+++ src/main/eta/Primes.hs
@@ -0,0 +1,5 @@
+module Primes where
+
+primes = filterPrime [2..]
+  where filterPrime (p:xs) =
+          p : filterPrime [x | x <- xs, x `mod` p /= 0]
