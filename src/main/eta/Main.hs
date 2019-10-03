8c5c11ff604e7ad3b4e9fe711ce407cf0fe552c6
Eunmin Kim <telepopsound@gmail.com>
2019-10-03 22:40:18 +0900

    Add basic s-expression example

--- src/main/eta/Main.hs
+++ src/main/eta/Main.hs
@@ -1,3 +1,20 @@
-import Primes
+data Exp = Num Double
+          | Lst [Exp]
+          | PlusFn
+          deriving (Show, Eq)
 
-main = putStrLn $ "The 101st prime is " ++ show (primes !! 100)
+value :: Exp -> Double
+value (Num n) = n
+
+eval :: Exp -> Exp
+eval (Lst (PlusFn:xs)) = Num $ sum (map
+                                    (\x -> case x of
+                                        (Num n) -> value x
+                                        ns -> value $ eval ns)
+                                    xs)
+
+eval x = x
+
+-- (+ 10 (+ 10 10))
+main =
+  putStrLn $ show (eval $ Lst [PlusFn, (Num 10), Lst [PlusFn, (Num 10), (Num 10)]])
--- src/main/eta/Primes.hs
+++
@@ -1,5 +0,0 @@
-module Primes where
-
-primes = filterPrime [2..]
-  where filterPrime (p:xs) =
-          p : filterPrime [x | x <- xs, x `mod` p /= 0]
