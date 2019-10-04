71747ae6c5df61e695f12228975a609c43f01b90
Eunmin Kim <telepopsound@gmail.com>
2019-10-05 00:12:52 +0900

    Apply Functor/Applicative to Nm

--- src/main/eta/Main.hs
+++ src/main/eta/Main.hs
@@ -1,20 +1,21 @@
-data Exp = Num Double
-          | Lst [Exp]
+data Exp a = Nm a
+          | Lst [Exp a]
           | PlusFn
           deriving (Show, Eq)
 
-value :: Exp -> Double
-value (Num n) = n
+instance Functor Exp where
+  fmap f (Nm a) = Nm (f a)
 
-eval :: Exp -> Exp
-eval (Lst (PlusFn:xs)) = Num $ sum (map
-                                    (\x -> case x of
-                                        (Num n) -> value x
-                                        ns -> value $ eval ns)
-                                    xs)
+-- only Nm
+instance Applicative Exp where  
+    pure = Nm
+    (Nm f) <*> nm = fmap f nm
+  
+eval :: (Num a) => Exp a -> Exp a
+eval (Lst (PlusFn:xs)) = foldl (\acc x -> (+) <$> acc <*> (eval x)) (Nm 0) xs
 
 eval x = x
 
 -- (+ 10 (+ 10 10))
 main =
-  putStrLn $ show (eval $ Lst [PlusFn, (Num 10), Lst [PlusFn, (Num 10), (Num 10)]])
+  putStrLn $ show (eval $ Lst [PlusFn, (Nm 10), Lst [PlusFn, (Nm 10), (Nm 10)]])
