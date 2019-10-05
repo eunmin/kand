a0a991c3c6f635b50c9b4dccc27b4d182e706da6
Eunmin Kim <telepopsound@gmail.com>
2019-10-05 23:06:54 +0900

    Add Symbol / Function expression

--- src/main/eta/Main.hs
+++ src/main/eta/Main.hs
@@ -1,21 +1,40 @@
-data Exp a = Nm a
-          | Lst [Exp a]
-          | PlusFn
-          deriving (Show, Eq)
+import qualified Data.Map as Map
 
-instance Functor Exp where
-  fmap f (Nm a) = Nm (f a)
+data Exp = Nm Double
+          | Fn ([Exp] -> Exp)
+          | Sym String
+          | Lst [Exp]
 
--- only Nm
-instance Applicative Exp where  
-    pure = Nm
-    (Nm f) <*> nm = fmap f nm
-  
-eval :: (Num a) => Exp a -> Exp a
-eval (Lst (PlusFn:xs)) = foldl (\acc x -> (+) <$> acc <*> (eval x)) (Nm 0) xs
+type Env = Map.Map String Exp
 
-eval x = x
+plus :: [Exp] -> Exp
+plus ((Nm x):(Nm y):[]) = Nm (x + y)
 
--- (+ 10 (+ 10 10))
+minus :: [Exp] -> Exp
+minus ((Nm x):(Nm y):[]) = Nm (x - y)
+
+eval :: Env -> Exp -> Exp
+eval env (Lst ((Fn f):args)) = f $ map (eval env) args
+eval env (Sym name) =
+  case x of
+    Just value -> value
+  where
+    x = Map.lookup name env
+    
+eval _ x = x
+
+instance Show Exp where
+  show = showExp
+
+showExp :: Exp -> String
+showExp (Nm n) = show n
+showExp (Fn f) = "function"
+showExp (Lst _) = "list"
+
+globalEnv = Map.fromList [("x", Nm 100)]
+
 main =
-  putStrLn $ show (eval $ Lst [PlusFn, (Nm 10), Lst [PlusFn, (Nm 10), (Nm 10)]])
+  putStrLn $ show (eval globalEnv expression)
+  where
+    -- (+ 10 (- x 10)) ;; 100
+    expression = Lst [(Fn plus), Nm 1, Lst [(Fn minus), Sym "x", Nm 1]]
