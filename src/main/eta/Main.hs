70f22f550726bcd1bac01682090ae4a7da0e02f3
Eunmin Kim <telepopsound@gmail.com>
2019-10-06 17:33:10 +0900

    Add Error data type

--- src/main/eta/Main.hs
+++ src/main/eta/Main.hs
@@ -4,20 +4,44 @@ data Exp = Nm Double
           | Fn ([Exp] -> Exp)
           | Sym String
           | Lst [Exp]
+          | Error String 
 
 type Env = Map.Map String Exp
 
+instance Semigroup Exp where
+  Error x <> Error y = Error (x ++ " " ++ y)
+
+instance Monoid Exp where
+  mempty = Error ""
+
 plus :: [Exp] -> Exp
 plus ((Nm x):(Nm y):[]) = Nm (x + y)
+plus _ = Error "Invalid args"
 
 minus :: [Exp] -> Exp
 minus ((Nm x):(Nm y):[]) = Nm (x - y)
+minus _ = Error "Invalid args"
 
 eval :: Env -> Exp -> Exp
-eval env (Lst ((Fn f):args)) = f $ map (eval env) args
+eval env (Lst (exp:exps)) =
+  if length errors == 0
+  then
+    case exp of
+      Fn f -> f args
+      Sym name -> case (eval env (Sym name)) of
+        Fn f -> f args
+        _ -> Error $ (show exp) ++ " is not function"
+      _ -> Error $ (show exp) ++ " is not function"
+  else
+    foldl (<>) mempty errors
+  where
+    args = map (eval env) exps
+    errors = filter (\exp -> case exp of { (Error _) -> True; _ -> False }) args
+
 eval env (Sym name) =
   case x of
     Just value -> value
+    Nothing -> Error ("Unable to resolve symbol " ++ name)
   where
     x = Map.lookup name env
     
@@ -30,11 +54,17 @@ showExp :: Exp -> String
 showExp (Nm n) = show n
 showExp (Fn f) = "function"
 showExp (Lst _) = "list"
+showExp (Sym name) = "symbol " ++ name
+showExp (Error message) = "Error: " ++ message
 
-globalEnv = Map.fromList [("x", Nm 100)]
+globalEnv = Map.fromList [
+  ("+", Fn plus),
+  ("-", Fn minus),
+  ("x", Nm 1)
+  ]
 
 main =
   putStrLn $ show (eval globalEnv expression)
   where
-    -- (+ 10 (- x 10)) ;; 100
-    expression = Lst [(Fn plus), Nm 1, Lst [(Fn minus), Sym "x", Nm 1]]
+    -- (+ 10 (- 100 10)) ;; 100
+    expression = Lst [Sym "+", Nm 10, Lst [Sym "-", Nm 100, Nm 10]]
