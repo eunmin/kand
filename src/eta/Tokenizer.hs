18972bfbdd05f65c91b3945797fa18d4af54275f
Eunmin Kim <telepopsound@gmail.com>
2019-11-21 22:23:42 +0900

    Split tokenize function

--- src/eta/Tokenizer.hs
+++ src/eta/Tokenizer.hs
@@ -23,12 +23,16 @@ stringToken s = TokenList [Token s]
 
 tokenize :: String -> String -> TokenList -> (TokenList, String)
 
-tokenize (x:xs) buffer result
-  | x == '(' = let (ast, rest) = tokenize xs "" EmptyTokenList
-               in tokenize rest "" (result <> TokenList [ast])
-  | x == ')' = ((result <> stringToken buffer), xs)                
-  | isSpace x = tokenize xs "" (result <> stringToken buffer)
-  | otherwise = tokenize xs (buffer ++ [x]) result
-  
-tokenize "" buffer result = ((result <> stringToken buffer), "")
+tokenize ('(':xs) buffer result =
+  let (ast, rest) = tokenize xs "" EmptyTokenList
+  in tokenize rest "" (result <> TokenList [ast])
 
+tokenize (')':xs) buffer result =
+  ((result <> stringToken buffer), xs)
+
+tokenize (x:xs) buffer result =
+  if isSpace x
+  then tokenize xs "" (result <> stringToken buffer)
+  else tokenize xs (buffer ++ [x]) result
+
+tokenize "" buffer result = ((result <> stringToken buffer), "")
