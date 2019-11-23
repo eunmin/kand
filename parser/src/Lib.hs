fd85b4ba5c465fbab6b385241a866d92a12a7731
Eunmin Kim <telepopsound@gmail.com>
2019-11-23 22:41:08 +0900

    Add Parser with parsec

--- parser/src/Lib.hs
+++ parser/src/Lib.hs
@@ -1,16 +1,2 @@
-module Lib
-    ( someFunc
-    , parseExp ) where
+module Lib where
 
-import Text.Parsec
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
-
-wordParser :: Parsec String () String
-wordParser = many (noneOf " ")
-
-wordsParser :: Parsec String () [String]
-wordsParser = (:) <$> wordParser <*> many (char ' ' *> wordParser)
-
-parseExp = parse wordsParser ""
