2cdeda96485022133b846af82e004d2ebe064dd9
Eunmin Kim <telepopsound@gmail.com>
2019-11-26 23:51:11 +0900

    Finding java reflection for eta

--- eta-kand/src/Kand/Reflect.hs
+++ eta-kand/src/Kand/Reflect.hs
@@ -1,7 +1,22 @@
-{-# LANGUAGE TypeFamilies, DataKinds, TypeOperators, FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies, DataKinds, TypeOperators, FlexibleContexts, OverloadedStrings #-}
 
-module Kand.Reflect ( newObject ) where
+module Kand.Reflect ( classForName
+                    , newObject
+                    , test ) where
 
 import Java
 
-foreign import java unsafe "@static org.apache.commons.lang3.reflect.Constructorutils.invokeConstructor" newObject :: Class a -> JObjectArray -> Java a c
+data JavaClass = JavaClass @java.lang.Class deriving Class
+
+foreign import java unsafe "@static java.lang.Class.forName" classForName :: String -> Java c JavaClass
+
+foreign import java unsafe "@static org.apache.commons.lang3.reflect.Constructorutils.invokeConstructor" newObject :: (a <: Object) => JavaClass -> JObjectArray -> Java c a
+
+type instance Inherits String = '[Object]
+
+test :: IO ()
+test = java $ do
+  cls <- classForName "java.lang.String"
+  args <- arrayFromList ["a"]
+  obj <- newObject cls args
+  return obj
