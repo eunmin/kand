bc7e00df559f335f13aa7504ef1beec466f24351
Eunmin Kim <telepopsound@gmail.com>
2019-11-27 23:13:00 +0900

    Replace JavaClass to JClass

--- eta-kand/src/Kand/Reflect.hs
+++ eta-kand/src/Kand/Reflect.hs
@@ -6,17 +6,15 @@ module Kand.Reflect ( classForName
 
 import Java
 
-data JavaClass = JavaClass @java.lang.Class deriving Class
+foreign import java unsafe "@static org.apache.commons.lang3.reflect.Constructorutils.invokeConstructor" newObject :: (a <: Object) => JClass a -> JObjectArray -> Java c a
 
-foreign import java unsafe "@static java.lang.Class.forName" classForName :: String -> Java c JavaClass
-
-foreign import java unsafe "@static org.apache.commons.lang3.reflect.Constructorutils.invokeConstructor" newObject :: (a <: Object) => JavaClass -> JObjectArray -> Java c a
+foreign import java unsafe "@static java.lang.Class.forName" forName :: String -> JClass a
 
 type instance Inherits String = '[Object]
 
 test :: IO ()
 test = java $ do
-  cls <- classForName "java.lang.String"
-  args <- arrayFromList ["a"]
-  obj <- newObject cls args
-  return obj
+    args <- arrayFromList ["a"]
+    return $ newObject (forName "java.lang.String") args
+
+
