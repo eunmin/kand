0f1a53132be1db3d157fd74744b6f527c26b5aa0
Eunmin Kim <telepopsound@gmail.com>
2019-11-30 10:39:36 +0900

    Add invokeMethod

--- eta-kand/src/Kand/Reflect.hs
+++ eta-kand/src/Kand/Reflect.hs
@@ -2,20 +2,23 @@
 
 module Kand.Reflect ( forName
                     , newObject
-                    , test
+                    , invokeMethod
                     ) where
 
 import Java
 
-foreign import java unsafe "@static org.apache.commons.lang3.reflect.ConstructorUtils.invokeConstructor" newObject :: (a <: Object) => JClass a -> JObjectArray -> Java c a
-
 foreign import java unsafe "@static java.lang.Class.forName" forName :: String -> Java c (JClass a)
 
-test :: IO JString
-test = java $ do
-  (arr :: JObjectArray) <- arrayFromList [superCast $ toJString "abc"]
-  (cls :: JClass JString) <- forName "java.lang.String"
-  obj <- newObject cls arr
-  return obj
+foreign import java unsafe "@static org.apache.commons.lang3.reflect.ConstructorUtils.invokeConstructor" newObject :: (a <: Object) => JClass a -> JObjectArray -> Java c a
+
+foreign import java unsafe "@static org.apache.commons.lang3.reflect.MethodUtils.invokeExtractMethod" invokeMethod :: (a <: Object, b <: Object) => a -> String ->  JObjectArray -> b
+-- (MethodUtils/invokeExactMethod obj method-name (into-array (map to-java args)))
+
+-- test :: IO JString
+-- test = java $ do
+--   (arr :: JObjectArray) <- arrayFromList [superCast $ toJString "abc"]
+--   (cls :: JClass JString) <- forName "java.lang.String"
+--   obj <- newObject cls arr
+--   return obj
 
 
