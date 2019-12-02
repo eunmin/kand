8fe6e2c56ad2f9f0ddadf1446afd4a5e86fce269
Eunmin Kim <telepopsound@gmail.com>
2019-12-02 17:07:48 +0900

    Add JBool

--- eta-kand/src/Kand/Reflect.hs
+++ eta-kand/src/Kand/Reflect.hs
@@ -4,6 +4,7 @@ module Kand.Reflect ( forName
                     , newObject
                     , invokeMethod
                     , test
+                    , booleanValue
                     ) where
 
 import Java
@@ -12,16 +13,26 @@ foreign import java unsafe "@static java.lang.Class.forName" forName :: String -
 
 foreign import java unsafe "@static org.apache.commons.lang3.reflect.ConstructorUtils.invokeConstructor" newObject :: (a <: Object) => JClass a -> JObjectArray -> Java c a
 
-foreign import java unsafe "@static org.apache.commons.lang3.reflect.MethodUtils.invokeExtractMethod" invokeMethod :: (a <: Object, b <: Object) => a -> String ->  JObjectArray -> b
+foreign import java unsafe "@static org.apache.commons.lang3.reflect.MethodUtils.invokeExactMethod" invokeMethod :: (a <: Object, b <: Object) => a -> String ->  JObjectArray -> Java c b
 
 foreign import java unsafe isEmpty :: Java JString Bool
 
-test :: String -> IO Bool
+foreign import java unsafe booleanValue :: Java JBool Bool
+
+data JBool = JBool @java.lang.Boolean deriving Class
+
+type instance Inherits JBool = '[Object]
+
+test :: String -> IO JBool
 test s = java $ do
   (arr :: JObjectArray) <- arrayFromList [superCast $ toJString s]
   (cls :: JClass JString) <- forName "java.lang.String"
   obj <- newObject cls arr
-  empty <- obj <.> isEmpty
-  return empty
+  (args :: JObjectArray) <- (arrayFromList [])
+  (o :: Object) <- invokeMethod obj "isEmpty" args
+  return $ unsafeCast o
+--  (jb :: JBool) <- 
+--  b <- jb <.> booleanValue
+
 
 
