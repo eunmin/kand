d6008ca92d3b9590019423dd72b8081219abbdb5
Eunmin Kim <telepopsound@gmail.com>
2019-12-03 20:18:46 +0900

    Fix bug (invokeMethod)

--- eta-kand/src/Kand/Reflect.hs
+++ eta-kand/src/Kand/Reflect.hs
@@ -4,6 +4,8 @@ module Kand.Reflect ( forName
                     , newObject
                     , invokeMethod
                     , test
+                    , test2
+                    , newBoolean
                     , booleanValue
                     ) where
 
@@ -17,22 +19,27 @@ foreign import java unsafe "@static org.apache.commons.lang3.reflect.MethodUtils
 
 foreign import java unsafe isEmpty :: Java JString Bool
 
-foreign import java unsafe booleanValue :: Java JBool Bool
-
 data JBool = JBool @java.lang.Boolean deriving Class
+
+foreign import java unsafe "@new" newBoolean :: Bool -> Java a JBool
+
+foreign import java unsafe booleanValue :: Java JBool Bool
 
 type instance Inherits JBool = '[Object]
 
-test :: String -> IO JBool
-test s = java $ do
+test :: String -> Java c JBool
+test s = do
   (arr :: JObjectArray) <- arrayFromList [superCast $ toJString s]
   (cls :: JClass JString) <- forName "java.lang.String"
   obj <- newObject cls arr
   (args :: JObjectArray) <- (arrayFromList [])
   (o :: Object) <- invokeMethod obj "isEmpty" args
   return $ unsafeCast o
---  (jb :: JBool) <- 
---  b <- jb <.> booleanValue
 
+test2 :: String -> IO Bool
+test2 s = java $ do
+  b <- test s
+  bb <- b <.> booleanValue
+  return bb
 
-
+  
