6a82463ab8fa8502a8f6a4eb17d2912db990aafa
Eunmin Kim <telepopsound@gmail.com>
2019-12-05 18:43:24 +0900

    Implementing general newInstace function

--- eta-kand/src/Kand/Reflect.hs
+++ eta-kand/src/Kand/Reflect.hs
@@ -3,11 +3,10 @@
 module Kand.Reflect ( forName
                     , newObject
                     , invokeMethod
-                    , test
                     , test2
                     , newBoolean
                     , booleanValue
-                    , lastIndexOf ) where
+                    , newInstance ) where
 
 import Java
 
@@ -27,28 +26,34 @@ foreign import java unsafe booleanValue :: Java JBool Bool
 
 type instance Inherits JBool = '[Object]
 
-test :: String -> Java c JBool
-test s = do
-  (arr :: JObjectArray) <- arrayFromList [superCast $ toJString s]
-  (cls :: JClass JString) <- forName "java.lang.String"
+-- test :: String -> Java c JBool
+-- test s = do
+--   (arr :: JObjectArray) <- arrayFromList [superCast $ toJString s]
+--   (cls :: JClass JString) <- forName "java.lang.String"
+--   obj <- newObject cls arr
+--   (args :: JObjectArray) <- (arrayFromList [])
+--   (o :: Object) <- invokeMethod obj "isEmpty" args
+--   return $ unsafeCast o
+
+-- lastIndexOf :: String -> String -> Int -> Java c JInteger
+-- lastIndexOf s str fromIndex = do
+--   (arr :: JObjectArray) <- arrayFromList [superCast $ toJString s]
+--   (cls :: JClass JString) <- forName "java.lang.String"
+--   obj <- newObject cls arr
+--   (args :: JObjectArray) <- arrayFromList [ superCast $ toJString str  
+--                                           , superCast $ (toJava fromIndex :: JInteger) ]
+--   (o :: Object) <- invokeMethod obj "lastIndexOf" args
+--   return $ unsafeCast o
+
+newInstance :: String -> [String] -> Java c JString
+newInstance className args = do
+  (arr :: JObjectArray) <- arrayFromList $ map (superCast . toJString) args
+  (cls :: JClass Object) <- forName className
   obj <- newObject cls arr
-  (args :: JObjectArray) <- (arrayFromList [])
-  (o :: Object) <- invokeMethod obj "isEmpty" args
-  return $ unsafeCast o
+  return $ unsafeCast obj
 
-test2 :: String -> IO Bool
-test2 s = java $ do
-  b <- test s
-  bb <- b <.> booleanValue
-  return bb
-
-lastIndexOf :: String -> String -> Int -> Java c JInteger
-lastIndexOf s str fromIndex = do
-  (arr :: JObjectArray) <- arrayFromList [superCast $ toJString s]
-  (cls :: JClass JString) <- forName "java.lang.String"
-  obj <- newObject cls arr
-  (args :: JObjectArray) <- arrayFromList [ superCast $ toJString str  
-                                          , superCast $ (toJava fromIndex :: JInteger) ]
-  (o :: Object) <- invokeMethod obj "lastIndexOf" args
-  return $ unsafeCast o
-
+test2 :: IO JString
+test2 = java $ do
+  obj <- newInstance "java.lang.String" ["a"] 
+  return obj
+  
