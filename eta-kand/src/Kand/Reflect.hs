743d50c4efc48287cb41f81e76388d782f5c3bcd
Eunmin Kim <telepopsound@gmail.com>
2019-12-14 17:18:51 +0900

    Support primitive args for invoke function

--- eta-kand/src/Kand/Reflect.hs
+++ eta-kand/src/Kand/Reflect.hs
@@ -1,22 +1,24 @@
 {-# LANGUAGE TypeFamilies, DataKinds, TypeOperators, FlexibleContexts, OverloadedStrings, ScopedTypeVariables, MultiParamTypeClasses #-}
+{-# LANGUAGE MagicHash #-}
 
-module Kand.Reflect ( forName
+module Kand.Reflect ( getClass
                     , newObject
                     , invokeMethod
                     , invoke
                     , test
                     , test2
+                    , test3
                     , newBoolean
                     , booleanValue
                     , newInstance ) where
 
 import Java
 
-foreign import java unsafe "@static java.lang.Class.forName" forName :: String -> Java c (JClass a)
+foreign import java unsafe "@static org.apache.commons.lang3.ClassUtils.getClass" getClassWithName :: String -> Java c (JClass a)
 
 foreign import java unsafe "@static org.apache.commons.lang3.reflect.ConstructorUtils.invokeConstructor" newObject :: (a <: Object) => JClass a -> JObjectArray -> Java c a
 
-foreign import java unsafe "@static org.apache.commons.lang3.reflect.MethodUtils.invokeExactMethod" invokeMethod :: (a <: Object, b <: Object) => a -> String ->  JObjectArray -> JClassArray -> Java c b
+foreign import java unsafe "@static org.apache.commons.lang3.reflect.MethodUtils.invokeExactMethod" invokeMethod :: (a <: Object, b <: Object, c <: Object) => a -> String ->  JObjectArray -> (JClassArray c) -> Java d b
 
 foreign import java unsafe isEmpty :: Java JString Bool
 
@@ -28,23 +30,21 @@ foreign import java unsafe booleanValue :: Java JBool Bool
 
 type instance Inherits JBool = '[Object]
 
-data JavaClass = JavaClass @java.lang.Class deriving Class
+data {-# CLASS "java.lang.Class[]" #-} JClassArray a = JClassArray (Object# (JClassArray a)) deriving Class
 
-data JClassArray = JClassArray @java.lang.Class[] deriving Class
-
-instance JArray JavaClass JClassArray
+instance JArray (JClass a) (JClassArray a)
 
 newInstance :: (a <: Object) => String -> [Object] -> Java c a
 newInstance className args = do
   arr <- arrayFromList args
-  (cls :: JClass Object) <- forName className
+  (cls :: JClass Object) <- getClassWithName className
   obj <- newObject cls arr
   return $ unsafeCast obj
 
 invoke :: (a <: Object) => Object -> String -> [Object] -> [String] -> Java c a
 invoke obj methodName args argClasses = do
   arr <- arrayFromList args
-  (argTypes :: [JClass Object]) <- mapM forName argClasses
+  (argTypes :: [JClass Object]) <- mapM getClassWithName argClasses
   typeArr <- arrayFromList argTypes
   r <- invokeMethod obj methodName arr typeArr
   return r
@@ -59,5 +59,11 @@ test s = java $ do
 test2 :: String -> IO JInteger
 test2 s = java $ do
   obj <- newInstance "java.lang.String" [(superCast $ toJString s)]
-  r <- invoke obj "indexOf" [ superCast $ (toJava (0 :: Int) :: JInteger) ] []
+  r <- invoke obj "indexOf" [ superCast $ (toJava ("a" :: String) :: JString) ] ["java.lang.String"]
+  return r
+
+test3 :: String -> IO JInteger
+test3 s = java $ do
+  obj <- newInstance "java.lang.String" [(superCast $ toJString s)]
+  r <- invoke obj "indexOf" [ superCast $ (toJava (65 :: Int) :: JInteger) ] ["int"]
   return r
