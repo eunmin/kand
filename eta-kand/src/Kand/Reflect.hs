be1bd4207f08991eca7da573bf2fcb27507f86a6
Eunmin Kim <telepopsound@gmail.com>
2019-12-15 20:29:50 +0900

    Rename functions

--- eta-kand/src/Kand/Reflect.hs
+++ eta-kand/src/Kand/Reflect.hs
@@ -1,44 +1,26 @@
 {-# LANGUAGE TypeFamilies, DataKinds, TypeOperators, FlexibleContexts, OverloadedStrings, ScopedTypeVariables, MultiParamTypeClasses #-}
 {-# LANGUAGE MagicHash #-}
 
-module Kand.Reflect ( getClass
-                    , newObject
-                    , invokeMethod
-                    , invoke
-                    , test
-                    , test2
-                    , test3
-                    , newBoolean
-                    , booleanValue
-                    , newInstance ) where
+module Kand.Reflect ( invoke
+                    , newObject ) where
 
 import Java
 
 foreign import java unsafe "@static org.apache.commons.lang3.ClassUtils.getClass" getClassWithName :: String -> Java c (JClass a)
 
-foreign import java unsafe "@static org.apache.commons.lang3.reflect.ConstructorUtils.invokeConstructor" newObject :: (a <: Object) => JClass a -> JObjectArray -> Java c a
+foreign import java unsafe "@static org.apache.commons.lang3.reflect.ConstructorUtils.invokeConstructor" newJavaObject :: (a <: Object) => JClass a -> JObjectArray -> Java c a
 
 foreign import java unsafe "@static org.apache.commons.lang3.reflect.MethodUtils.invokeExactMethod" invokeMethod :: (a <: Object, b <: Object, c <: Object) => a -> String ->  JObjectArray -> (JClassArray c) -> Java d b
-
-foreign import java unsafe isEmpty :: Java JString Bool
-
-data JBool = JBool @java.lang.Boolean deriving Class
-
-foreign import java unsafe "@new" newBoolean :: Bool -> Java a JBool
-
-foreign import java unsafe booleanValue :: Java JBool Bool
-
-type instance Inherits JBool = '[Object]
 
 data {-# CLASS "java.lang.Class[]" #-} JClassArray a = JClassArray (Object# (JClassArray a)) deriving Class
 
 instance JArray (JClass a) (JClassArray a)
 
-newInstance :: (a <: Object) => String -> [Object] -> Java c a
-newInstance className args = do
+newObject :: (a <: Object) => String -> [Object] -> Java c a
+newObject className args = do
   arr <- arrayFromList args
   (cls :: JClass Object) <- getClassWithName className
-  obj <- newObject cls arr
+  obj <- newJavaObject cls arr
   return $ unsafeCast obj
 
 invoke :: (a <: Object) => Object -> String -> [Object] -> [String] -> Java c a
@@ -49,21 +31,14 @@ invoke obj methodName args argClasses = do
   r <- invokeMethod obj methodName arr typeArr
   return r
 
-test :: String -> IO Bool
-test s = java $ do
-  obj <- newInstance "java.lang.String" [(superCast $ toJString s)]
-  r <- invoke obj "isEmpty" [] []
-  r2 <- r <.> booleanValue
-  return r2
-
-test2 :: String -> IO JInteger
-test2 s = java $ do
-  obj <- newInstance "java.lang.String" [(superCast $ toJString s)]
+example1 :: String -> IO JInteger
+example1 s = java $ do
+  obj <- newObject "java.lang.String" [(superCast $ toJString s)]
   r <- invoke obj "indexOf" [ superCast $ (toJava ("a" :: String) :: JString) ] ["java.lang.String"]
   return r
 
-test3 :: String -> IO JInteger
-test3 s = java $ do
-  obj <- newInstance "java.lang.String" [(superCast $ toJString s)]
+example2 :: String -> IO JInteger
+example2 s = java $ do
+  obj <- newObject "java.lang.String" [(superCast $ toJString s)]
   r <- invoke obj "indexOf" [ superCast $ (toJava (65 :: Int) :: JInteger) ] ["int"]
   return r
